4bdfa274b00985eeab72a29050f194a77f5b5c50
Harsh sahu <sahuharsh164@gmail.com>
2024-06-05 15:42:45 +0530

    add register login page

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "./pages/Login";
 import HomePage from "./pages/Home";
+import RegisterPage from "./pages/Register";
 
 export const router=  createBrowserRouter([
     {
@@ -11,5 +12,9 @@ export const router=  createBrowserRouter([
     {
         path:'/login',
         element:<LoginPage/>
+    },
+    {
+        path:'/register',
+        element:<RegisterPage/>
     }
 ])
