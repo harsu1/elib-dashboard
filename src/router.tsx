1f335e60906a50de60fbd2f9d5fcd19392742113
Harsh sahu <sahuharsh164@gmail.com>
2024-06-06 10:32:28 +0530

    dashboard layout

--- src/router.tsx
+++ src/router.tsx
@@ -1,27 +1,39 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
 import LoginPage from "./pages/Login";
 import HomePage from "./pages/Home";
 import RegisterPage from "./pages/Register";
 import DashboardLayout from "./layouts/DashboardLayout";
+import BookPage from "./pages/BookPage";
 
-export const router=  createBrowserRouter([
-    {
-
-        path:'/',
-        element: <DashboardLayout/>,
-        children:[
-            {path:'home',
-            element:<HomePage/>}
-        ]
-
-       
-    },
-    {
-        path:'/login',
-        element:<LoginPage/>
-    },
-    {
-        path:'/register',
-        element:<RegisterPage/>
-    }
-])
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/dashboard/home" />,
+  },
+  {
+    path: "dashboard",
+    element: <DashboardLayout />,
+    children: [
+      {
+        path: "home",
+        element: <HomePage />,
+      },
+      {
+        path: "books",
+        element: <BookPage />,
+      },
+    //   {
+    //     path: 'books/create',
+    //     element: <CreateBook />,
+    // },
+    ],
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+]);
