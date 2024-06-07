423a83279edbf390a6c657a558c2e180b7ecaa04
Harsh sahu <sahuharsh164@gmail.com>
2024-06-07 12:00:19 +0530

    add book button

--- src/pages/BookPage.tsx
+++ src/pages/BookPage.tsx
@@ -1,8 +1,13 @@
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from '@/components/ui/breadcrumb'
-import { getBooks } from '@/http/api'
-import { useQuery } from '@tanstack/react-query'
-import React from 'react'
-import { Button } from "@/components/ui/button"
+import { Badge } from '@/components/ui/badge';
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
+import { Button } from '@/components/ui/button';
 import {
     Card,
     CardContent,
@@ -10,136 +15,138 @@ import {
     CardFooter,
     CardHeader,
     CardTitle,
-  } from "@/components/ui/card"
-  import {
+} from '@/components/ui/card';
+import {
     DropdownMenu,
-     DropdownMenuContent,
+    DropdownMenuContent,
     DropdownMenuItem,
     DropdownMenuLabel,
-     DropdownMenuTrigger,
-  } from "@/components/ui/dropdown-menu"
-  import {
+    DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import {
     Table,
     TableBody,
     TableCell,
     TableHead,
     TableHeader,
     TableRow,
-  } from "@/components/ui/table"
-   import { Badge } from "@/components/ui/badge"
-  import {    MoreHorizontal } from 'lucide-react';
-import { Book } from '@/types'
+} from '@/components/ui/table';
+import { getBooks } from '@/http/api';
+import { Book } from '@/types';
+import { useQuery } from '@tanstack/react-query';
+import { CirclePlus, MoreHorizontal } from 'lucide-react';
+import { Link } from 'react-router-dom';
 
-const BookPage = () => {
-    const {data, isLoading, isError}=useQuery({
-        queryKey:['books'],
+const BooksPage = () => {
+    // todo: add loading spinner, and error message
+    // @ts-ignore
+
+    const { data, isLoading, isError } = useQuery({
+        queryKey: ['books'],
         queryFn: getBooks,
-        staleTime:10000,
-    })
-  return (
-    <div>
-      <Breadcrumb>
-  <BreadcrumbList>
-    <BreadcrumbItem>
-      <BreadcrumbLink href="/dashboard/home">Home</BreadcrumbLink>
-    </BreadcrumbItem>
-    <BreadcrumbSeparator />
-      <BreadcrumbItem>
-      <BreadcrumbPage>Books</BreadcrumbPage>
-    </BreadcrumbItem>
-  </BreadcrumbList>
-</Breadcrumb>
+        staleTime: 10000, // in Milli-seconds
+    });
 
-<Card  className='mt-6'>
+    return (
+        <div>
+            <div className="flex items-center justify-between">
+                <Breadcrumb>
+                    <BreadcrumbList>
+                        <BreadcrumbItem>
+                            <BreadcrumbLink href="/dashboard/home">Home</BreadcrumbLink>
+                        </BreadcrumbItem>
+                        <BreadcrumbSeparator />
+                        <BreadcrumbItem>
+                            <BreadcrumbPage>Books</BreadcrumbPage>
+                        </BreadcrumbItem>
+                    </BreadcrumbList>
+                </Breadcrumb>
+                <Link to="/dashboard/books/create">
+                    <Button>
+                        <CirclePlus size={20} />
+                        <span className="ml-2">Add book</span>
+                    </Button>
+                </Link>
+            </div>
+
+            <Card className="mt-6">
                 <CardHeader>
-                  <CardTitle>Books</CardTitle>
-                  <CardDescription>
-                    Manage your Books and view their sales performance.
-                  </CardDescription>
+                    <CardTitle>Books</CardTitle>
+                    <CardDescription>
+                        Manage your books and view their sales performance.
+                    </CardDescription>
                 </CardHeader>
                 <CardContent>
-                  <Table>
-                    <TableHeader>
-
-                      <TableRow>
-                        <TableHead className="hidden w-[100px] sm:table-cell">
-                          <span className="sr-only">Image</span>
-                        </TableHead>
-                        <TableHead>Title</TableHead>
-                        <TableHead>Genre</TableHead>
-                         <TableHead className="hidden md:table-cell">
-                          Author name
-                        </TableHead>
-                        <TableHead className="hidden md:table-cell">
-                          Created at
-                        </TableHead>
-                        <TableHead>
-                          <span className="sr-only">Actions</span>
-                        </TableHead>
-                      </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                        {data?.data.map((book: Book)=>{
-                            return(
-                                 <TableRow key={book._id}>
-                        <TableCell className="hidden sm:table-cell">
-                          <img
-                            alt={ book.title}
-                            className="aspect-square rounded-md object-cover"
-                            height="64"
-                            src={book.coverImage}
-                            width="64"
-                          />
-                        </TableCell>
-                        <TableCell className="font-medium">
-                         {book.title}
-                        </TableCell>
-                        <TableCell>
-                          <Badge variant="outline">{book.genre}</Badge>
-                        </TableCell>
-                         <TableCell className="hidden md:table-cell">
-                       {book.author.name}
-                        </TableCell>
-                        <TableCell className="hidden md:table-cell">
-                         {book.createdAt}
-                        </TableCell>
-                        <TableCell>
-                          <DropdownMenu>
-                            <DropdownMenuTrigger asChild>
-                              <Button
-                                aria-haspopup="true"
-                                size="icon"
-                                variant="ghost"
-                              >
-                                <MoreHorizontal className="h-4 w-4" />
-                                <span className="sr-only">Toggle menu</span>
-                              </Button>
-                            </DropdownMenuTrigger>
-                            <DropdownMenuContent align="end">
-                              <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                              <DropdownMenuItem>Edit</DropdownMenuItem>
-                              <DropdownMenuItem>Delete</DropdownMenuItem>
-                            </DropdownMenuContent>
-                          </DropdownMenu>
-                        </TableCell>
-                      </TableRow>
-                            );
-                        })}
-                     
-                       
-                    </TableBody>
-                  </Table>
+                    <Table>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead className="hidden w-[100px] sm:table-cell">
+                                    <span className="sr-only">Image</span>
+                                </TableHead>
+                                <TableHead>Title</TableHead>
+                                <TableHead>Genre</TableHead>
+                                <TableHead className="hidden md:table-cell">Author name</TableHead>
+                                <TableHead className="hidden md:table-cell">Created at</TableHead>
+                                <TableHead>
+                                    <span className="sr-only">Actions</span>
+                                </TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {data?.data.map((book: Book) => {
+                                return (
+                                    <TableRow key={book._id}>
+                                        <TableCell className="hidden sm:table-cell">
+                                            <img
+                                                alt={book.title}
+                                                className="aspect-square rounded-md object-cover"
+                                                height="64"
+                                                src={book.coverImage}
+                                                width="64"
+                                            />
+                                        </TableCell>
+                                        <TableCell className="font-medium">{book.title}</TableCell>
+                                        <TableCell>
+                                            <Badge variant="outline">{book.genre}</Badge>
+                                        </TableCell>
+                                        <TableCell className="hidden md:table-cell">
+                                            {book.author.name}
+                                        </TableCell>
+                                        <TableCell className="hidden md:table-cell">
+                                            {book.createdAt}
+                                        </TableCell>
+                                        <TableCell>
+                                            <DropdownMenu>
+                                                <DropdownMenuTrigger asChild>
+                                                    <Button
+                                                        aria-haspopup="true"
+                                                        size="icon"
+                                                        variant="ghost">
+                                                        <MoreHorizontal className="h-4 w-4" />
+                                                        <span className="sr-only">Toggle menu</span>
+                                                    </Button>
+                                                </DropdownMenuTrigger>
+                                                <DropdownMenuContent align="end">
+                                                    <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                                                    <DropdownMenuItem>Edit</DropdownMenuItem>
+                                                    <DropdownMenuItem>Delete</DropdownMenuItem>
+                                                </DropdownMenuContent>
+                                            </DropdownMenu>
+                                        </TableCell>
+                                    </TableRow>
+                                );
+                            })}
+                        </TableBody>
+                    </Table>
                 </CardContent>
                 <CardFooter>
-                  <div className="text-xs text-muted-foreground">
-                    Showing <strong>1-10</strong> of <strong>32</strong>{" "}
-                    products
-                  </div>
+                    <div className="text-xs text-muted-foreground">
+                        Showing <strong>1-10</strong> of <strong>32</strong> products
+                    </div>
                 </CardFooter>
-              </Card>
+            </Card>
+        </div>
+    );
+};
 
-    </div>
-  )
-}
-
-export default BookPage
+export default BooksPage;
--- src/router.tsx
+++ src/router.tsx
@@ -5,44 +5,45 @@ import RegisterPage from "./pages/Register";
 import DashboardLayout from "./layouts/DashboardLayout";
 import BookPage from "./pages/BookPage";
 import AuthLayout from "./layouts/AuthLayout";
+import CreateBook from "./pages/CreateBook";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Navigate to="/dashboard/home" />,
-  },
-  {
-    path: "dashboard",
-    element: <DashboardLayout />,
-    children: [
-      {
-        path: "home",
-        element: <HomePage />,
-      },
-      {
-        path: "books",
-        element: <BookPage />,
-      },
-    //   {
-    //     path: 'books/create',
-    //     element: <CreateBook />,
-    // },
-    ],
-  },
-  {
-     path: '/auth',
-     element: <AuthLayout/>,
-     children:[
-        {
-            path: "login",
-            element: <LoginPage />,
-          },
-          {
-            path: "register",
-            element: <RegisterPage />,
-          },
-     ]
-
-  },
-   
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Navigate to="/dashboard/home" />,
+    },
+    {
+        path: 'dashboard',
+        element: <DashboardLayout />,
+        children: [
+            {
+                path: 'home',
+                element: <HomePage />,
+            },
+            {
+                path: 'books',
+                element: <BookPage />,
+            },
+            {
+                path: 'books/create',
+                element: <CreateBook />,
+            },
+        ],
+    },
+    {
+        path: '/auth',
+        element: <AuthLayout />,
+        children: [
+            {
+                path: 'login',
+                element: <LoginPage />,
+            },
+            {
+                path: 'register',
+                element: <RegisterPage />,
+            },
+        ],
+    },
 ]);
+
+export default router;
